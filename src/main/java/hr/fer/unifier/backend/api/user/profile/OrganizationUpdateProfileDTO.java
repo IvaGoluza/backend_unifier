40b040801c9868e56ff08f49773df3c389a882d3
Dean Trkulja <deantrkulja2001@gmail.com>
2024-03-12 17:22:28 +0100

    Dorađeno ažuriranje profila

--- src/main/java/hr/fer/unifier/backend/api/user/profile/OrganizationUpdateProfileDTO.java
+++ src/main/java/hr/fer/unifier/backend/api/user/profile/OrganizationUpdateProfileDTO.java
@@ -1,19 +1,13 @@
 package hr.fer.unifier.backend.api.user.profile;
 
-import hr.fer.unifier.backend.api.location.AddressRequestDTO;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Optional;
 
 @Data
 public class OrganizationUpdateProfileDTO {
-    private String name;
-    private String profileDescription;
-    private String email;
-    private String mobilePhone;
-    private AddressRequestDTO address;
-    private String type;
-    private String url;
-    private String oib;
-    private List<String> workArea;
+    private Optional<String> profileDescription;
+    private Optional<String> url;
+    private Optional<List<String>> workArea;
 }
--- src/main/java/hr/fer/unifier/backend/mapper/UserMapper.java
+++ src/main/java/hr/fer/unifier/backend/mapper/UserMapper.java
@@ -4,32 +4,21 @@ import hr.fer.unifier.backend.api.user.OrganizationDTO;
 import hr.fer.unifier.backend.api.user.PersonDTO;
 import hr.fer.unifier.backend.api.user.UserCardInfoDTO;
 import hr.fer.unifier.backend.api.user.profile.OrganizationProfileDTO;
-import hr.fer.unifier.backend.api.user.profile.OrganizationUpdateProfileDTO;
 import hr.fer.unifier.backend.api.user.profile.PersonProfileDTO;
-import hr.fer.unifier.backend.api.user.profile.UserUpdateProfileDTO;
 import hr.fer.unifier.backend.api.user.register.UserResponseDTO;
 import hr.fer.unifier.backend.db.user.entity.Organization;
 import hr.fer.unifier.backend.db.user.entity.Person;
 import hr.fer.unifier.backend.db.user.entity.User;
 import hr.fer.unifier.backend.db.user.entity.UserCardInfo;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring", uses = {AddressMapper.class, CommonMapper.class})
 public interface UserMapper {
     UserResponseDTO toUserResponseDTO(User user);
-
     UserCardInfoDTO toUserCardInfo(UserCardInfo userCardInfo);
-
     PersonProfileDTO toPersonProfileDTO(Person person);
     OrganizationProfileDTO toOrganizationProfileDTO(Organization organization);
-
-    void updatePerson(@MappingTarget Person person, UserUpdateProfileDTO userUpdateProfileDTO);
-
-    void updateOrganization(@MappingTarget Organization organization, OrganizationUpdateProfileDTO userProfileDTO);
-
     OrganizationDTO toOrganizationDTO(Organization organization);
-
     PersonDTO toPersonDTO(Person person);
 }
