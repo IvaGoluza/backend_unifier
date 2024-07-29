374da5256d8fdf30472965fdb8c526d836eee95b
Dean Trkulja <deantrkulja2001@gmail.com>
2024-07-29 19:14:01 +0200

    Dorađen spring security - dio sa iznimkama

--- src/main/java/hr/fer/unifier/backend/config/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/hr/fer/unifier/backend/config/exception/RestResponseEntityExceptionHandler.java
@@ -13,11 +13,22 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler(value = {EntityNotFoundException.class, ResponseStatusException.class, ExpiredJwtException.class})
-    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
-        if (ex instanceof ResponseStatusException re)
+    protected ResponseEntity<Object> handleKnownExceptions(RuntimeException ex, WebRequest request) {
+        if (ex instanceof ResponseStatusException re) {
             return handleExceptionInternal(re, re.getMessage(), re.getHeaders(), re.getStatusCode(), request);
-         return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        } else if (ex instanceof EntityNotFoundException) {
+            return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        } else if (ex instanceof ExpiredJwtException) {
+            return handleExceptionInternal(ex, "JWT token has expired", new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
+        } else {
+            return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+        }
     }
 
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        return handleExceptionInternal(ex, "An unexpected error occurred", new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
 }
